1a68e12399510b63441330c11b7970606e477561
masakiii03 <y.masaki.0317@gmail.com>
2023-05-24 15:28:42 +0900

    spring cloud config の追加

--- gateway-sample/src/main/java/com/example/gatewaysample/GatewaySampleApplication.java
+++ gateway-sample/src/main/java/com/example/gatewaysample/GatewaySampleApplication.java
@@ -1,13 +1,27 @@
 package com.example.gatewaysample;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RefreshScope
 public class GatewaySampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewaySampleApplication.class, args);
+	}
+
+	@Value("${gw.value}")
+	private String value;
+
+	@GetMapping("/value")
+	public String getValue() {
+		return value;
 	}
 
 }
