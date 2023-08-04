0ebf55cfff2695d1c7044bf9259a1abea508546e
masakiii03 <y.masaki.0317@gmail.com>
2023-08-04 13:01:39 +0900

    metadataがrefreshされないよう修正

---
+++ client4/src/main/java/com/example/client4/config/EurekaMetadataConfig.java
@@ -0,0 +1,25 @@
+package com.example.client4.config;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class EurekaMetadataConfig {
+
+    @Autowired
+    private ApplicationInfoManager aim;
+
+    @PostConstruct
+    public void init() {
+        Map<String, String> map = aim.getInfo().getMetadata();
+        map.put("version", "1");
+        // map.put("version", "2");
+    }
+
+}
