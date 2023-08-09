2ce698df3bd63f4684b6066d5eca34e534dc6867
masakiii03 <y.masaki.0317@gmail.com>
2023-08-09 17:56:35 +0900

    metadata設定方法の変更

--- client4/src/main/java/com/example/client4/config/EurekaMetadataConfig.java
+++ client4/src/main/java/com/example/client4/config/EurekaMetadataConfig.java
@@ -1,25 +1,25 @@
-package com.example.client4.config;
+// package com.example.client4.config;
 
-import java.util.Map;
+// import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Component;
 
-import com.netflix.appinfo.ApplicationInfoManager;
+// import com.netflix.appinfo.ApplicationInfoManager;
 
-import jakarta.annotation.PostConstruct;
+// import jakarta.annotation.PostConstruct;
 
-@Component
-public class EurekaMetadataConfig {
+// @Component
+// public class EurekaMetadataConfig {
 
-    @Autowired
-    private ApplicationInfoManager aim;
+// @Autowired
+// private ApplicationInfoManager aim;
 
-    @PostConstruct
-    public void init() {
-        Map<String, String> map = aim.getInfo().getMetadata();
-        map.put("version", "1");
-        // map.put("version", "2");
-    }
+// @PostConstruct
+// public void init() {
+// Map<String, String> map = aim.getInfo().getMetadata();
+// map.put("version", "1");
+// // map.put("version", "2");
+// }
 
-}
+// }
